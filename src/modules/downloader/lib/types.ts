2beae7b69dd15fe7b2cf6f93166e30d94a8260f7
Rebiz <guardiansofspartax@gmail.com>
2026-05-22 03:46:43 +0530

    feat: implement full repository-based downloader ecosystem

---
+++ src/modules/downloader/lib/types.ts
@@ -0,0 +1,15 @@
+/**
+ * Every Ember module in a remote repository must have an info.json file
+ * in its root directory to be recognized by the Downloader.
+ */
+export interface ModuleInfo {
+  name: string;
+  author: string[];
+  description: string;
+  short: string;
+  version: string;
+  requirements?: string[]; // npm packages to install
+  tags?: string[];
+  min_bot_version?: string;
+  hidden?: boolean;
+}
